d5f00b1dbea6c72f18c7b37d10c44b7b4a83d43b
Momoware <hadnazzar@users.noreply.github.com>
2016-12-21 00:45:53 +0200

    Update listports.js

--- listports.js
+++ listports.js
@@ -1,9 +1,9 @@
 var serialport = require('serialport');
 
- 
-// list serial ports:
+//list serial ports:
 serialport.list(function (err, ports) {
   ports.forEach(function(port) {
+   //Console log each connection name
     console.log(port.comName);
   });
 });
