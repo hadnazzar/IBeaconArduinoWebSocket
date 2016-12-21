b4341860c0efd020ccb1d4239ec5d34df06e2891
Momoware <hadnazzar@users.noreply.github.com>
2016-12-21 10:29:49 +0200

    Added new properties to serial port

--- listports.js
+++ listports.js
@@ -1,9 +1,27 @@
+// example port information 
+/*{
+  comName: '/dev/cu.usbmodem1421',
+  manufacturer: 'Arduino (www.arduino.cc)',
+  serialNumber: '757533138333964011C1',
+  pnpId: undefined,
+  locationId: '0x14200000',
+  vendorId: '0x2341',
+  productId: '0x0043'
+}
+*/
+
 var serialport = require('serialport');
 
 //list serial ports:
 serialport.list(function (err, ports) {
   ports.forEach(function(port) {
    //Console log each connection name
-    console.log(port.comName);
+    console.log("ComName: " + port.comName);
+    console.log("Manufacturer: " +port.manufacturer);
+    console.log("SerialNumber: " + port.serialNumber);
+    console.log("PnpId: " + port.pnpId);
+    console.log("LocationId: " + port.locationId);
+    console.log("VendorId: " + port.vendorId);
+    console.log("ProductId: " + port.productId);
   });
 });
